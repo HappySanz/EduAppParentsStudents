e3d7e501e90fcb1b6aeea07be6fc2f7adedfa2d3
Narendar <naren.happysanz@gmail.com>
2017-04-11 17:17:35 +0530

    package additions screen design

---
+++ app/src/main/java/com/palprotech/eduappparentsstudents/activity/ClassTestHomeworkActivity.java
@@ -0,0 +1,17 @@
+package com.palprotech.eduappparentsstudents.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.palprotech.eduappparentsstudents.R;
+
+/**
+ * Created by Narendar on 07/04/17.
+ */
+
+public class ClassTestHomeworkActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classtest_homework);
+    }
+}
---
+++ app/src/main/java/com/palprotech/eduappparentsstudents/activity/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.palprotech.eduappparentsstudents.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.palprotech.eduappparentsstudents.R;
+
+/**
+ * Created by Narendar on 05/04/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
